f7ecf330970df41e3a9f89009ec0ea2e4ea1bc68
Andrew Tredennick <atredenn@gmail.com>
2015-04-28 14:36:54 -0600

    Initial commit of community_synchrony project.

---
+++ R/community_synchrony.R
@@ -0,0 +1,13 @@
+##  Function that runs all synchrony metrics given a dataset. The input
+##    are the genet-level time series for a specific site. If quad-years
+##    need to be removed, this should be done before feeding the data
+##    to this function.
+
+##  Outputs: -- time series of percent cover by species
+##           -- time series of per capita growth rates by speciues
+##           -- table of synchrony and stability metrics for the community
+
+##  Author:       Andrew Tredennick
+##  Email:        atredenn@gmail.com
+##  Last update:  4.28.2015
+
