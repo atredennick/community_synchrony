e770ea465202d7c6a639b30fe3c7275cd435dbeb
Andrew Tredennick <atredenn@gmail.com>
2015-04-30 16:11:16 -0600

    adds crowding list output to file

--- inst/call_crowding_fxns.R
+++ inst/call_crowding_fxns.R
@@ -5,11 +5,14 @@ path_to_data <- "../data/"
 
 site_list <- list.files("../data/")
 removes <- c(grep("*.csv", site_list),
-             grep("Climate", site_list))
+             grep("Climate", site_list),
+             grep("Crowding", site_list))
 site_list <- site_list[-removes]
 
 growth_alphas <- read.csv(paste(path_to_data, "alpha_list_growth.csv", sep=""))
 survival_alphas <- read.csv(paste(path_to_data, "alpha_list_survival.csv", sep=""))
+
+crowd_list <- list()
 
 # Loop through sites and get crowding for growth and survival
 #   save each output in the respective data folder
@@ -26,4 +29,14 @@ for(do_site in site_list){
                                       data_path = path_to_data,
                                       alphas = tmp_alpha_surv,
                                       vital_rate = "survival")
+  
+  # Save output
+  grow_list[[do_site]] <- tmp_crowd_grow
+  surv_list[[do_site]] <- tmp_crowd_surv
 }
+
+names(grow_list) <- site_list
+names(surv_list) <- site_list
+
+saveRDS(grow_list, "../data/Crowding/crowd_grow_list.RDS")
+saveRDS(surv_list, "../data/Crowding/crowd_survival_list.RDS")
