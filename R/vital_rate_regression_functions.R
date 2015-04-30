9403e57ddcefac58e3887d2ef51258e9d9ce1fe0
Andrew Tredennick <atredenn@gmail.com>
2015-04-30 11:25:35 -0600

    initial commit of vital rate functions

---
+++ R/vital_rate_regression_functions.R
@@ -0,0 +1,12 @@
+##  Functions using INLA to get vital rate regression parameters
+
+#' Estimate growth regression coefficients using INLA
+#' 
+#' @author Andrew Tredennick
+#' @param dataframe Time series dataframe of genet sizes at t0 and t1 with 
+#'                  neighborhood crowding covariate for each observation.
+#' @return Dataframe with regression coefficients.
+
+get_growth_params <- function(dataframe){
+  
+}
