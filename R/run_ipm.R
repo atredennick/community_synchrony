17c46296507d97ce46c5171595a6dcfe6aa8f6dd
Andrew Tredennick <atredenn@gmail.com>
2015-05-04 14:51:46 -0600

    adds demographic stochasticity flag and IPM simulation loop

--- R/run_ipm.R
+++ R/run_ipm.R
@@ -12,10 +12,11 @@
 #' @param Rpars Recruitment regression parameters for focal site and species.
 #' @param Spars Survival regression parameters for focal site and species.
 #' @param Gpars Recruitment regression parameters for focal site and species.
+#' @param demographic_stochasticity TRUE/FALSE flag for including demographic stoachasticity or not.
 
 run_ipm <- function(A=10000, tlimit=2500, burn.in=500, sppList,
                     Nyrs, constant=FALSE, bigM, maxSize,
-                    Rpars, Spars, Gpars){
+                    Rpars, Spars, Gpars, demographic_stochasticity=FALSE){
   library(IPMdoit)
   library(boot)
   library(mvtnorm)
@@ -29,7 +30,85 @@ run_ipm <- function(A=10000, tlimit=2500, burn.in=500, sppList,
     Spars$intcpt.yr[]=0;Spars$slope.yr[]=0    
   }
   
+  # Build initial vectors and matrices
+  n_spp <- length(spp_list)
+  inits <- make_inits_ipm(n_spp = n_spp, 
+                          iter_matrix_dims = iter_matrix_dims, 
+                          max_size = max_size)
   
+  ##
+  ## Run simulation -----------------------------------------------------------
+  ##
+  # initial population density vector
+  nt <- inits$v
   
+  # loop through species really quick to set low initial densities
+  for(i in 1:n_spp) nt[[i]][]=0.001
+  new.nt <- nt #set initial density vector to be fed into IPM
   
+  # set up matrix to record cover
+  covSave <- matrix(NA,tlimit,n_spp)
+  covSave[1,] <- sum_cover(inits$v,nt,inits$h,A)
+  
+  # set up list to store size distributions
+  sizeSave <- list(NULL)
+  for(i in 1:n_spp){
+    sizeSave[[i]] <- matrix(NA,length(inits$v[[i]]),(tlimit))
+    sizeSave[[i]][,1] <- nt[[i]]/sum(nt[[i]])
+  }
+  
+  # initial densities 
+  Nsave <- matrix(NA,tlimit,n_spp)
+  Nsave[1,] <- sum_N(nt,inits$h)
+  
+  yrSave <- rep(NA,tlimit)
+  
+  # Loop through simulation times and iterate population
+  pb <- txtProgressBar(min=2, max=tlimit, char="+", style=3, width=65)
+  for (t in 2:(tlimit)){
+    #draw from observed year effects
+    allYrs <- c(1:Nyrs)
+    doYear <- sample(allYrs,1)
+    yrSave[t] <- doYear
+    
+    #get recruits per area
+    cover <- covSave[t-1,]
+    N <- Nsave[t-1,]
+    recs_per_area <- get_rpa(Rpars,cover,doYear)
+    
+    #calculate size-specific crowding
+    alphaG <- Gpars$alpha 
+    alphaS <- Spars$alpha 
+    if(NoOverlap_Inter==F){#T: heterospecific genets cannot overlap; F: overlap allowed
+      crowd_list <- crowd_overlap(A, N, inits$vt, inits$h, alphaG, alphaS, inits$WmatG, inits$WmatS,
+                                  n_spp, inits$Ctot, inits$Cr, inits$b.r, inits$expv, inits$r.U, inits$v.r, inits$v)
+    }else{
+      crowd_list <- crowd_no_overlap(A, inits$vt, inits$h, alphaG, alphaS, inits$WmatG, inits$WmatS,
+                                     n_spp, inits$Ctot, inits$Cr, inits$b.r, inits$expv, inits$r.U, inits$v.r,
+                                     inits$size_range)
+    } # end NoOverlap if
+    
+    for(doSpp in 1:n_spp){  
+      if(cover[doSpp]>0){    
+        # Make kernels and project
+        K_matrix=make_K_matrix(inits$v[[doSpp]],crowd_list$WmatG[[doSpp]],
+                               crowd_list$WmatS[[doSpp]],
+                               Rpars,recs_per_area,Gpars,Spars,
+                               doYear,doSpp,inits$h,
+                               demo_stoch=demographic_stochasticity) 
+        
+        new.nt[[doSpp]]=K_matrix%*%nt[[doSpp]] 
+        sizeSave[[doSpp]][,t]=new.nt[[doSpp]]/sum(new.nt[[doSpp]])  
+      }    
+    } # next species
+    
+    nt=new.nt 
+    covSave[t,]=sum_cover(inits$v,nt,inits$h,A)  # store the cover as cm^2/cm^2
+    Nsave[t,]=sum_N(nt,inits$h)
+    
+    setTxtProgressBar(pb, t)
+    flush.console()
+    if(sum(is.na(nt))>0) browser()  
+  } # next time step
+  return(covSave)
 } # end of function
