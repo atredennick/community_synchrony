bccdf8b48987bf2929ab17867c36323860d8d333
Andrew Tredennick <atredenn@gmail.com>
2015-05-05 10:13:28 -0600

    adds survival params import fxn

--- R/import_regression_params_fxns.R
+++ R/import_regression_params_fxns.R
@@ -36,3 +36,50 @@ format_growth_params <- function(do_site, species_list, Nyrs, Gdata){
 } # end function
 
 
+
+
+#' Import and format survival parameters for IPM
+#' 
+#' @author Andrew Tredennick
+#' @param do_site Focal site (character scalar).
+#' @param species_list Character vector of four letter species' codes.
+#' @param Nyrs Number of random effects years.
+#' @param Sdata Survival parameters matrix from regression output.
+
+format_survival_params <- function(do_site, species_list, Nyrs, Sdata){
+  Nspp <- length(species_list)
+  Spars <- list(intcpt=rep(NA,Nspp),intcpt.yr=matrix(0,Nyrs,Nspp),
+                slope=rep(NA,Nspp),slope.yr=matrix(0,Nyrs,Nspp),
+                nb=matrix(0,Nspp,Nspp),intcpt.gr=matrix(0,6,Nspp),
+                alpha=matrix(NA,Nspp,Nspp))
+  
+  for(i in 1:Nspp){
+    Spars$intcpt[i] <- Sdata$Intercept[1]
+    
+    tmp <- which(names(Sdata)=="Group")
+    if(length(tmp)>0) 
+      Spars$intcpt.gr[,i] <- Sdata$Group[!is.na(Sdata$Group)] # get spatial average
+    
+    tmp <- which(names(Sdata)=="Intercept.yr")
+    if(length(tmp)>0) 
+      Spars$intcpt.yr[,i] <- Sdata$Intercept.yr
+    
+    Spars$slope[i] <- Sdata$logarea[1]
+    
+    # random effects on slope
+    tmp <- which(names(Sdata)=="logarea.yr")
+    if(length(tmp)>0)
+      Spars$slope.yr[,i] <- Sdata[,tmp]
+    
+    # get competition coefficients
+    tmp <- paste("crowd",1:length(sppList),sep="")
+    tmp <- which(is.element(names(Sdata),tmp))
+    if(length(tmp)>0)
+      Spars$nb[i,] <- as.numeric(Sdata[1,tmp])
+    
+    Spars$alpha[i,]=Sdata$alpha[1:length(sppList)]
+  } # next i
+  return(Spars)
+} # end of function
+
+
