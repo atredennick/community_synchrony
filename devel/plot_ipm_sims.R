b1b4d1ee0fba14e73fca21bfd72119ffaba6d8cb
Andrew Tredennick <atredenn@gmail.com>
2015-11-02 11:13:06 -0700

    updates ipm synchrony fig with errarbars from bootstrap

--- devel/plot_ipm_sims.R
+++ devel/plot_ipm_sims.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(reshape2)
+library(plyr)
 library(communitySynchrony)
 
 output_list <- readRDS("../results/ipm_comp_nocomp_sims.RDS")
@@ -5,45 +8,46 @@ mlist <- melt(output_list)
 colnames(mlist)[1:3] <- c("year", "species", "cover")
 sites <- unique(mlist$L1)
 sims <- unique(mlist$L2)
-pgr_list <- list()
+synch_df <- data.frame(site=NA, experiment=NA, bootnum=NA, synch=NA)
+boots <- 100
+num_iters <- 100
 for(dosite in sites){
   tmp_data <- subset(mlist, L1==dosite)
-  if(dosite=="Idaho"){
-    tmp_data <- subset(tmp_data, species!="ARTR")
-  }
-  within_site_list <- list()
   for(dosim in sims){
     tmpsim <- subset(tmp_data, L2==dosim)[c("year", "species", "cover")]
-    tmpsynch <- get_ipm_synchrony(tmpsim)
-    tmp_pgr_synch <- as.numeric(tmpsynch$pgr_synchrony["obs"])
-    within_site_list[[dosim]] <- tmp_pgr_synch
-  }
-  pgr_list[[dosite]] <- within_site_list
-}
-pgr_synch <- melt(pgr_list)
-expected_synch <- table_data[,c(1,3)]
-expected_synch$L2 <- "E(SYNCH)"
-colnames(expected_synch) <- c("L1", "value", "L2")
-expected_synch <- expected_synch[,c("value", "L2", "L1")]
-expected_synch[which(expected_synch[,"L1"]=="New Mexico"),"L1"] <- "NewMexico"
+    for(i in 1:boots){
+      begin_year <- sample(x = 1:(max(tmpsim$year)-num_iters), 1)
+      end_year <- begin_year+num_iters
+      tmp <- subset(tmpsim, year %in% begin_year:end_year)
+      tmpsynch <- get_ipm_synchrony(tmp)
+      tmp_pgr_synch <- as.numeric(tmpsynch$pgr_synchrony["obs"])
+      tmp_df <- data.frame(site=dosite, experiment=dosim, 
+                           bootnum=i, synch=tmp_pgr_synch)
+      synch_df <- rbind(synch_df, tmp_df)
+    }# end boots loop
+  }# end experiment/sim loop
+}# end site loop
 
-pgr_synch <- rbind(pgr_synch, expected_synch)
+synch_df <- synch_df[2:nrow(synch_df),]
+agg_synch <- ddply(synch_df, .(site, experiment), summarise,
+                   mean_synch = mean(synch),
+                   up_synch = quantile(synch, 0.95),
+                   lo_synch = quantile(synch, 0.05))
 
-# ggplot(pgr_synch, aes(x=L2, y=value, fill=L2))+
-#   geom_bar(stat="identity", position=position_dodge(0.9), color="grey")+
-#   facet_wrap("L1", scales="free_x")+
-#   scale_fill_manual(values = c("grey45", "steelblue", "slateblue4"))+
-#   guides(fill=FALSE)+
-#   scale_y_continuous(limits=c(0,1))+
-#   xlab("Source of stochasticity")+
-#   ylab("Species synchrony")
-
-ggplot(pgr_synch, aes(x=L2, y=value, color=L1, group=L1))+
+ipm_pgr_plot <- ggplot(agg_synch, aes(x=experiment, y=mean_synch, color=site, group=site))+
   geom_line()+
-  geom_point(size=5)+
+  geom_errorbar(aes(ymin=lo_synch, ymax=up_synch), width=0.05)+
+  geom_point(size=2)+
   scale_color_manual(values = c("grey45", "steelblue", "slateblue4", "darkorange", "purple"),
                      name = "Site")+
   scale_y_continuous(limits=c(0,1))+
+  scale_x_discrete(labels=c("F-INTER", "F-NoINTER"))+
   xlab("Simulation")+
   ylab("Species synchrony")+
+  facet_wrap("site", nrow=1)+
+  guides(color=FALSE)+
   theme_bw()
+
+png("../docs/components/ipm_pgr_plot.png", width=8.5, height=2.5, units="in", res=200)
+print(ipm_pgr_plot)
+dev.off()
