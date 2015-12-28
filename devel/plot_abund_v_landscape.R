a9a2efe00666fac750af9beb5b933240fb2381f9
Andrew Tredennick <atredenn@gmail.com>
2015-12-28 15:20:07 -0700

    updates ibm figure with burn in removal

--- devel/plot_abund_v_landscape.R
+++ devel/plot_abund_v_landscape.R
@@ -1,28 +1,13 @@
-## Script to plot IBM results
+##
+##  Plots mean abundance as a function of landscape size
+##  From IBM simulations
+##
 
-##  Author:       Andrew Tredennick
-##  Email:        atredenn@gmail.com
-##  Date created: 9-22-2015
-
-# Clear workspace 
-rm(list=ls(all=TRUE))
+rm(list=ls())
 
 totSims <- 50      # number of simulations per site 
 totT <- 100      # time steps of simulation
 burn.in <- 25    # time steps to discard before calculating cover values
-site_colors <- c("grey45", "steelblue", "slateblue4", "darkorange", "purple")
-
-####
-####  Load libraries -----------------------------------------------------------
-####
-library(ggplot2)
-library(reshape2)
-library(plyr)
-library(synchrony)
-library(gridExtra)
-
-### Function for inverse of %in%
-"%w/o%" <- function(x, y) x[!x %in% y] # x without y
 
 
 ####
@@ -52,7 +37,7 @@ rm(stringlist)
 
 
 output_df <- data.frame(experiment=NA, site=NA, expansion=NA, 
-                        run=NA, pgr_synch=NA, abund_synch=NA)
+                        run=NA, meanabund=NA)
 
 for(do_exp in unique(files_df$experiment)){
   exp_files <- files_df[which(files_df$experiment==do_exp),"filename"]
@@ -108,26 +93,10 @@ for(do_exp in unique(files_df$experiment)){
           species_names <- species_names[species_names!="Cov"]
           num_spp <- ncol(ts.tmp)
           ts.tmp$timestep <- c(1:nrow(ts.tmp))
-          
-          # Transform to per capita growth rates
-          lagts <- ts.tmp
-          lagts$lagtimestep <- lagts$timestep+1
-          colnames(lagts)[1:(num_spp+1)] <- paste0(colnames(lagts)[1:(num_spp+1)],"_t0") 
-          mergedts <- merge(ts.tmp, lagts, by.x="timestep", by.y="lagtimestep")
-          transitions <- nrow(mergedts)
-          obs_gr <- matrix(nrow=transitions, ncol=num_spp)
-          name_ids1 <- which(colnames(mergedts) %in% paste0("Cov.",species_names))
-          name_ids2 <- which(colnames(mergedts) %in% paste0("Cov.",species_names, "_t0"))
-          for(ii in 1:transitions){
-            obs_gr[ii,] <- as.numeric(log(mergedts[ii,name_ids1]/mergedts[ii,name_ids2]))
-          }
-          
           colids <- which(colnames(tmpsynch) %in% paste0("Cov.",species_names))
-          synch.abund <- as.numeric(community.sync(tmpsynch[burn.in:totT,colids])[1])
-          synch.pgr <- as.numeric(community.sync(obs_gr)[1])
+          mean.abund <- colMeans(tmpsynch[burn.in:totT,colids])
           tmpD <- data.frame(experiment=do_exp, site=do_site, expansion=i, 
-                             run=count, pgr_synch=synch.pgr, 
-                             abund_synch=synch.abund)
+                             run=count, meanabund=mean.abund)
           output_df <- rbind(output_df, tmpD)
           count <- count+1
         } # end loop over coexistence runs
@@ -137,44 +106,10 @@ for(do_exp in unique(files_df$experiment)){
     }# end plot size sim loop
     
   }# end site loop
-
+  
 }# end experiment loop
 
 
 
-# Remove NA first row
-output_df <- output_df[2:nrow(output_df),]
-colnames(output_df)[5:6] <- c("Per capita growth rate", "Cover (%)")
-synch_df <- melt(output_df, id.vars = c("experiment", "site", "expansion", "run"))
-colnames(synch_df) <- c("experiment", "site", "expansion", "run", 
-                        "typesynch", "synch")
 
-# Check for single datapoints
-allcombo <- ddply(synch_df, .(experiment, site, expansion, typesynch), summarise,
-                  nums = length(run))
-singles <- allcombo[which(allcombo$nums<10),]
-singles$remove="yes"
-test <- merge(synch_df, singles, by=c("experiment", "site", "expansion", "typesynch"), all = TRUE)
-ids.out <- which(test$remove=="yes")
-synch_dfplot <- test[-ids.out,]
-
-### Make plot and save
-ibm_plot <- ggplot(synch_dfplot, aes(x=expansion, y=synch, color=experiment))+
-  geom_point(alpha=0.5)+
-  stat_smooth(se=FALSE, method="lm", size=0.7)+
-  scale_color_manual(values = c("steelblue", "slateblue4", "darkorange", "darkred"),
-                     name="Simulation \nType",
-                     labels=c("C-INTER", "C-NoINTER",
-                              "F-INTER", "F-NoINTER"))+
-  scale_y_continuous(limits=c(0,1))+
-  xlab(expression(paste("Simulated landscape size (", m^2, ")")))+
-  ylab("Community synchrony")+
-  facet_grid(typesynch~site)+
-  theme_bw()
-
-setwd("~/Repos/community_synchrony/devel")
-png("../docs/components/ibm_sims_fig.png", height = 5, 
-    width = 10, units = "in", res=150)
-print(ibm_plot)
-dev.off()
 
