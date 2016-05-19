ecd72a19ecf27f1d258793be212bbfa58fd3c9a4
Andrew Tredennick <atredenn@gmail.com>
2016-05-19 15:51:43 -0600

    adds demographic variance figure as well

--- runscripts/calc_species_variances.R
+++ runscripts/calc_species_variances.R
@@ -12,7 +12,7 @@ site_colors <- c("grey45", "steelblue", "slateblue4", "darkorange", "purple")
 
 
 ####
-####  MAIN TEXT FIGURES 1-2
+####  SUPPLEMENTARY FIGURE FOR SPECIES' ENVIRONMENTAL VARIANCES
 ####
 ##  Read in IPM results ---------
 output_list <- readRDS("../results/ipm_comp_nocomp_sims.RDS")
@@ -47,7 +47,124 @@ dev.off()
 
 
 
+####
+####  SUPPLEMENTAL FIGURE FOR SPECIES' DEMOGRAPHIC VARIANCES
+####
+totSims <- 100   # number of simulations per site 
+totT <- 100      # time steps of simulation
+burn.in <- 25    # time steps to discard before calculating cover values
+site_colors <- c("grey45", "steelblue", "slateblue4", "darkorange", "purple")
+
+####
+####  Load libraries -----------------------------------------------------------
+####
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(synchrony)
+library(gridExtra)
+
+### Function for inverse of %in%
+"%w/o%" <- function(x, y) x[!x %in% y] # x without y
 
 
+####
+####  Read in data and setup up file lists -------------------------------------
+####
+# setwd("../results/ibm_sims/")
+setwd("../../../../../Volumes/A02046115/ibm_synch/results/")
+all_files <- list.files()
+constinterfiles <- all_files[grep("constinter", all_files)]
+constnointerfiles <- all_files[grep("constnointer", all_files)]
+fluctinterfiles <- all_files[grep("fluctinter", all_files)]
+fluctnointerfiles <- all_files[grep("fluctnointer", all_files)]
+exp_vector <- c("constinter", "constnointer", "fluctinter", "fluctnointer")
+files_df <- data.frame(experiment = rep(exp_vector, each=length(constinterfiles)),
+                       filename = c(constinterfiles,
+                                    constnointerfiles,
+                                    fluctinterfiles,
+                                    fluctnointerfiles))
 
+
+####
+####  Loop through experiment, sites, and runs ---------------------------------
+####
+
+stringlist <- strsplit(fluctinterfiles, split = "_")
+site_names <- unique(sapply(stringlist, "[[", 2))
+rm(stringlist)
+
+
+png("../../../../../Users/atredenn/Repos/community_synchrony/docs/components/demographic_variances.png", width = 10, height=3, units="in", res=100)
+par(mfrow=c(1,5), las=3)
+for(do_exp in "constnointer"){
+  exp_files <- files_df[which(files_df$experiment==do_exp),"filename"]
+  
+  for(do_site in site_names){ # loop over sites
+    tmp_files <- exp_files[grep(do_site, exp_files)]
+    nsims <- length(tmp_files)
+    
+    for(i in 5){ # loop over plot size simulations
+      tmp <- readRDS(as.character(tmp_files[i]))
+      tmp <- as.data.frame(tmp, row.names = c(1:nrow(tmp)))
+      cover.columns <- grep("Cov.", colnames(tmp))
+      
+      # Get rid of ARTR column for Idaho, for now
+      # if(do_site=="Idaho"){cover.columns <- cover.columns[2:length(cover.columns)]}
+      
+      end.tmp <- subset(tmp, time==totT)
+      extinct <- character(nrow(end.tmp))
+      extinct[] <- "no" # creates extinct storage vector 
+      
+      for(jj in 1:nrow(end.tmp)){ # loop over simulations within plot size
+        tmpjj <- subset(end.tmp, run==jj)
+        endcover <- tmpjj[,cover.columns]
+        
+        # Set extinct to "yes" if no runs coexist
+        if(nrow(endcover)==0){extinct[jj]<-"yes"}
+        
+        # Nested if/then for coexistence runs
+        if(nrow(endcover)>0){
+          if(length(which(endcover==0))>0){extinct[jj]<-"yes"} 
+        } # end if/then for extinction flagging
+        
+      } # end loop for sims within plot size
+      
+      # Create tmp dataframe for merging with main data
+      ext.df <- data.frame(run=c(1:nrow(end.tmp)), extinct=extinct)
+      tmp <- merge(tmp, ext.df)
+      tmp4synch <- subset(tmp, extinct=="no")
+      
+      # If the tmp dataframe is empty, no update
+      if(nrow(tmp4synch)==0){output_df <- output_df}
+      
+      # If the dataframe is not empty, get synchrony for coexistence runs
+      if(nrow(tmp4synch)>0){
+        runs <- unique(tmp4synch$run)
+        synch.run <- numeric(length(runs))
+        count <- 1 # set counter for indexing
+        
+        tmp_vars <- matrix(nrow=length(runs), ncol=length(cover.columns))
+        for(k in runs){ # loop over coexistence runs
+          tmpsynch <- subset(tmp4synch, run==k)
+          ts.tmp <- tmpsynch[burn.in:totT,cover.columns]
+          species_names <- unlist(strsplit(colnames(ts.tmp), "[.]"))
+          species_names <- species_names[species_names!="Cov"]
+          num_spp <- ncol(ts.tmp)
+          colids <- which(colnames(tmpsynch) %in% paste0("Cov.",species_names))
+          tmp_vars[k,] <- apply(tmpsynch[burn.in:totT,colids], 2, "sd")
+        } # end loop over coexistence runs
+        
+      } # end if/then for coexistence runs
+      
+    }# end plot size sim loop
+    
+    colnames(tmp_vars) <- species_names
+    boxplot(tmp_vars, main=do_site,  ylab="demographic variance", outline=F)
+    
+  }# end site loop
+  
+}# end experiment loop
+
+dev.off()
 
