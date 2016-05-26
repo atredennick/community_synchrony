ab9709fdf074e7f0d37f8224d12d3a9cbc83f181
Andrew Tredennick <atredenn@gmail.com>
2016-05-26 10:19:08 -0600

    updates parameter corr table generator

--- devel/make_paramcorr_tables.R
+++ devel/make_paramcorr_tables.R
@@ -12,14 +12,16 @@ all_files <- tmp_files[grep(".RDS", tmp_files)]
 if(grep("surv_grow_paramcorrs", list.files("../results/")) != 0){
   file.remove("../results/surv_grow_paramcorrs.txt")
 }
-
+newpages <- seq(3,length(all_files), by=3)
 for(i in 1:length(all_files)){
   tmp_corrs <- readRDS(paste0(in_dir,all_files[i]))
   vital <- strsplit(all_files[i], "_")[[1]][1]
   site <- strsplit(all_files[i], "_")[[1]][2]
   species <- strsplit(all_files[i], "_")[[1]][3]
-  cap <- paste(site,vital,species)
-  cat(print(xtable(tmp_corrs, digits=-2, caption = cap), caption.placement = "top"), file = "../results/surv_grow_paramcorrs.txt", append = TRUE, sep=" ")
+  cap <- paste0("Parameter correlations for site = ", site, ", vital rate = ",vital, ", and species = ",species,".")
+  cat(print(xtable(tmp_corrs, digits=-2, caption = cap), caption.placement = "top", comment=FALSE), file = "../results/surv_grow_paramcorrs.txt", append = TRUE, sep="\n")
+  cat("\n\n", file = "../results/surv_grow_paramcorrs.txt", append = TRUE)
+  if(i %in% newpages){cat("\\newpage{} \n\n", file = "../results/surv_grow_paramcorrs.txt", append = TRUE)}
 }
 
 
