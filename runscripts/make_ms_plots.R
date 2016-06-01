012739d6ee83c2680e0053bae85bb4df6f049ce8
Andrew Tredennick <atredenn@gmail.com>
2016-06-01 16:34:01 -0600

    adds cv vs synchrony figure for SI

--- runscripts/make_ms_plots.R
+++ runscripts/make_ms_plots.R
@@ -357,32 +357,210 @@ for(dosite in sites){
   }# end experiment/sim loop
 }# end site loop
 
-g1 <- ggplot(synch_df, aes(x=abund_synch, y=abund_cv, color=site))+
-  geom_point(shape=19, alpha=0.1)+
-  geom_point(shape=1, alpha=0.4)+
-  stat_smooth(method="lm", se=FALSE, size=1)+
-  facet_wrap("experiment")+
-  scale_x_continuous(limits=c(0,1))+
-  scale_color_manual(values=site_colors, name="")+
+
+##  Read in IBM results ---
+totSims <- 100   # number of simulations per site 
+totT <- 100      # time steps of simulation
+burn.in <- 25    # time steps to discard before calculating cover values
+
+### Function for inverse of %in%
+"%w/o%" <- function(x, y) x[!x %in% y] # x without y
+
+setwd("../../../../../Volumes/A02046115/ibm_synch/results/")
+all_files <- list.files()
+constinterfiles <- all_files[grep("constinter", all_files)]
+constnointerfiles <- all_files[grep("constnointer", all_files)]
+fluctinterfiles <- all_files[grep("fluctinter", all_files)]
+fluctnointerfiles <- all_files[grep("fluctnointer", all_files)]
+exp_vector <- c("constinter", "constnointer", "fluctinter", "fluctnointer")
+files_df <- data.frame(experiment = rep(exp_vector, each=length(constinterfiles)),
+                       filename = c(constinterfiles,
+                                    constnointerfiles,
+                                    fluctinterfiles,
+                                    fluctnointerfiles))
+
+
+####
+####  Loop through experiment, sites, and runs ---------------------------------
+####
+stringlist <- strsplit(fluctinterfiles, split = "_")
+site_names <- unique(sapply(stringlist, "[[", 2))
+rm(stringlist)
+
+
+output_df <- list()
+
+for(do_exp in unique(files_df$experiment)){
+  exp_files <- files_df[which(files_df$experiment==do_exp),"filename"]
+  
+  for(do_site in site_names){ # loop over sites
+    tmp_files <- exp_files[grep(do_site, exp_files)]
+    nsims <- length(tmp_files)
+    
+    for(i in 5){ # just do largest plot size, 5
+      tmp <- readRDS(as.character(tmp_files[i]))
+      tmp <- as.data.frame(tmp, row.names = c(1:nrow(tmp)))
+      cover.columns <- grep("Cov.", colnames(tmp))
+      
+      # Get rid of ARTR column for Idaho, for now
+      # if(do_site=="Idaho"){cover.columns <- cover.columns[2:length(cover.columns)]}
+      
+      end.tmp <- subset(tmp, time==totT)
+      extinct <- character(nrow(end.tmp))
+      extinct[] <- "no" # creates extinct storage vector 
+      
+      for(jj in 1:nrow(end.tmp)){ # loop over simulations within plot size
+        tmpjj <- subset(end.tmp, run==jj)
+        endcover <- tmpjj[,cover.columns]
+        
+        # Set extinct to "yes" if no runs coexist
+        if(nrow(endcover)==0){extinct[jj]<-"yes"}
+        
+        # Nested if/then for coexistence runs
+        if(nrow(endcover)>0){
+          if(length(which(endcover==0))>0){extinct[jj]<-"yes"} 
+        } # end if/then for extinction flagging
+        
+      } # end loop for sims within plot size
+      
+      # Create tmp dataframe for merging with main data
+      ext.df <- data.frame(run=c(1:nrow(end.tmp)), extinct=extinct)
+      tmp <- merge(tmp, ext.df)
+      tmp4synch <- subset(tmp, extinct=="no")
+      
+      # If the tmp dataframe is empty, no update
+      if(nrow(tmp4synch)==0){output_df <- output_df}
+      
+      # If the dataframe is not empty, get synchrony for coexistence runs
+      if(nrow(tmp4synch)>0){
+        runs <- unique(tmp4synch$run)
+        synch.run <- numeric(length(runs))
+        count <- 1 # set counter for indexing
+        
+        for(k in runs){ # loop over coexistence runs
+          tmpsynch <- subset(tmp4synch, run==k)
+          ts.tmp <- tmpsynch[burn.in:totT,cover.columns]
+          species_names <- unlist(strsplit(colnames(ts.tmp), "[.]"))
+          species_names <- species_names[species_names!="Cov"]
+          num_spp <- ncol(ts.tmp)
+          ts.tmp$timestep <- c(1:nrow(ts.tmp))
+          
+          # Transform to per capita growth rates
+          lagts <- ts.tmp
+          lagts$lagtimestep <- lagts$timestep+1
+          colnames(lagts)[1:(num_spp+1)] <- paste0(colnames(lagts)[1:(num_spp+1)],"_t0") 
+          mergedts <- merge(ts.tmp, lagts, by.x="timestep", by.y="lagtimestep")
+          transitions <- nrow(mergedts)
+          obs_gr <- matrix(nrow=transitions, ncol=num_spp)
+          name_ids1 <- which(colnames(mergedts) %in% paste0("Cov.",species_names))
+          name_ids2 <- which(colnames(mergedts) %in% paste0("Cov.",species_names, "_t0"))
+          for(ii in 1:transitions){
+            obs_gr[ii,] <- as.numeric(log(mergedts[ii,name_ids1]/mergedts[ii,name_ids2]))
+          }
+          
+          colids <- which(colnames(tmpsynch) %in% paste0("Cov.",species_names))
+          synch.abund <- as.numeric(community.sync(tmpsynch[burn.in:totT,colids])[1])
+          cv.abund <- sd(rowSums(tmpsynch[burn.in:totT,colids])) / mean(rowSums(tmpsynch[burn.in:totT,colids]))
+          synch.pgr <- as.numeric(community.sync(obs_gr)[1])
+          tmpD <- data.frame(experiment=do_exp, site=do_site, expansion=i, 
+                             run=count, pgr_synch=synch.pgr, 
+                             abund_synch=synch.abund, abund_cv=cv.abund)
+          output_df <- rbind(output_df, tmpD)
+          count <- count+1
+        } # end loop over coexistence runs
+        
+      } # end if/then for coexistence runs
+      
+    }# end plot size sim loop
+    
+  }# end site loop
+  
+}# end experiment loop
+
+
+### Combine IPM and IBM results
+plot_cv_df <- rbind(output_df[,c("site", "experiment", "pgr_synch", "abund_synch", "abund_cv")],
+                    synch_df[,c("site", "experiment", "pgr_synch", "abund_synch", "abund_cv")])
+
+site_names <- as.character(unique(plot_cv_df$site))
+nsites <- length(site_names)
+site_labels <- site_names
+site_labels[which(site_labels=="NewMexico")] <- "New Mexico"
+site_labels_order <- paste0(c(2,5,3,4,1), site_labels)
+site_labels <- site_labels[order(site_labels_order)]
+
+site_order_nums <- c(2,5,3,4,1)
+plot_cv_df$sites_order <- NA
+for(i in 1:length(site_names)){
+  do_site <- sort(site_names)[i]
+  plot_cv_df[which(plot_cv_df$site==do_site), "sites_order"] <- paste0(site_order_nums[i],do_site)
+}
+
+exp_labs <- c("No E.S.", "No Comp. + No E.S.", "No D.S.", "No Comp. + No D.S.", "All Drivers", "No Comp.")
+
+
+exp_colors <- c("#c7533b",
+                "#8e4cbe",
+                "#66a659",
+                "#ae517a",
+                "#9c7e40",
+                "#6984b3")
+
+library(gridExtra)
+
+g1 <- ggplot(plot_cv_df, aes(x=abund_synch, y=abund_cv, color=experiment))+
+  geom_point(shape=19, alpha=0.4)+
+  geom_point(shape=1, alpha=0.6)+
+  stat_smooth(method="lm", se=FALSE, size=1, linetype=1)+
+  facet_wrap("sites_order", nrow=1)+
+  scale_x_continuous(limits=c(0,1), labels=c("0","0.25","0.50","0.75","1"))+
+  scale_color_manual(values=exp_colors, name="", labels=exp_labs)+
+  # scale_fill_manual(values=c("lightcoral", "skyblue"), name="")+
   xlab("Synchrony of percent cover")+
   ylab("CV of total community cover")+
   theme_few()+
-  theme(legend.position=c(0.1,0.8),
-        legend.background = element_rect(fill = NA))+
+  guides(fill=FALSE)+
   theme(legend.key=element_rect(size=1),
-        legend.key.size = unit(0.5, "lines"))
+        legend.key.size = unit(0.8, "lines"))
 
-g2 <- ggplot(synch_df, aes(x=pgr_synch, y=abund_cv, color=site))+
-  geom_point(shape=19, alpha=0.1)+
-  geom_point(shape=1, alpha=0.4)+
-  stat_smooth(method="lm", se=FALSE, size=1)+
-  facet_wrap("experiment")+
-  scale_x_continuous(limits=c(0,1))+
-  scale_color_manual(values=site_colors, name="")+
+g2 <- ggplot(plot_cv_df, aes(x=pgr_synch, y=abund_cv, color=experiment))+
+  geom_point(shape=19, alpha=0.4)+
+  geom_point(shape=1, alpha=0.6)+
+  stat_smooth(method="lm", se=FALSE, size=1, linetype=1)+
+  facet_wrap("sites_order", nrow=1)+
+  scale_x_continuous(limits=c(0,1), labels=c("0","0.25","0.50","0.75","1"))+
+  scale_color_manual(values=exp_colors, name="", labels=exp_labs)+
+  # scale_fill_manual(values=c("lightcoral", "skyblue"), name="")+
   xlab("Synchrony of per capita growth rates")+
   ylab("CV of total community cover")+
   theme_few()+
-  guides(color=FALSE)
+  guides(fill=FALSE)+
+  theme(legend.key=element_rect(size=1),
+        legend.key.size = unit(0.8, "lines"))
 
-library(gridExtra)
+setwd("~/Repos/community_synchrony/docs/components/")
+png("cv_vs_synchrony.png", height=5, width=12, units = "in", res = 72)
 grid.arrange(g1, g2, nrow=2)
+dev.off()
+
+
+
+### Plot average CV versus average synchrony
+# cv_synch_agg <- ddply(plot_cv_df, .(sites_order,experiment), summarise,
+#                       avg_pgr_synch = mean(pgr_synch),
+#                       avg_abund_synch = mean(abund_synch),
+#                       avg_abund_cv = mean(abund_cv))
+# 
+# ggplot(cv_synch_agg, aes(x=avg_abund_synch, y=avg_abund_cv, color=experiment))+
+#   geom_point(shape=19, alpha=0.4)+
+#   geom_point(shape=1, alpha=0.6)+
+#   facet_wrap("sites_order", nrow=1)+
+#   scale_x_continuous(limits=c(0,1), labels=c("0","0.25","0.50","0.75","1"))+
+#   scale_color_manual(values=exp_colors, name="", labels=exp_labs)+
+#   # scale_fill_manual(values=c("lightcoral", "skyblue"), name="")+
+#   xlab("Synchrony of per capita growth rates")+
+#   ylab("CV of total community cover")+
+#   theme_few()+
+#   guides(fill=FALSE)+
+#   theme(legend.key=element_rect(size=1),
+#         legend.key.size = unit(0.8, "lines"))
