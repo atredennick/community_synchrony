ca5bb25ac4d3eee908b3d117eee2c30d6bfc6c36
Andrew Tredennick <atredenn@gmail.com>
2016-06-09 15:20:39 -0600

    adds new scripts and functions for yearly competition coefficients

---
+++ R/vital_rate_regression_functions_yrlycomp.R
@@ -0,0 +1,288 @@
+##  Functions using INLA to get vital rate regression parameters
+
+#' Estimate growth regression coefficients using INLA
+#' 
+#' @author Andrew Tredennick
+#' @param dataframe Time series dataframe of genet sizes at t0 and t1 with 
+#'                  neighborhood crowding covariate for each observation.
+#' @param crowd_mat Matrix of crowding indices for each observation. Matrix
+#'                  dimensions are nrow = number of observations,
+#'                  ncol = number of species.
+#' @param alphas Vector of length(species) of vital rate specific alpha values.
+#' @return Dataframe with named regression coefficients.
+
+get_growth_params_yrlcomp <- function(dataframe, crowd_mat, alpha){
+  D <- dataframe
+  D$logarea.t0 <- log(D$area.t0)
+  D$logarea.t1 <- log(D$area.t1)
+  D$quad <- as.character(D$quad)
+  
+  crowd = crowd_mat 
+  crowd[crowd<1e-99]=0 # make really small crowding indices 0
+  
+  D$yearW1 <- 100+D$yearID # for random year effect on crowding
+  D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+  D$yearW3 <- 100+D$yearW2 # for random year effect on crowding
+  D$yearW4 <- 100+D$yearW3 # for random year effect on crowding
+  
+  
+  library(INLA)
+  # Set up ID variables for INLA random effects
+  D$yearID <- D$year+max(D$year) # for random year offset on intercept
+  D$GroupID <- as.numeric(D$Group)
+  
+  if(ncol(crowd)==2) {
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    
+    formula <- logarea.t1 ~ logarea.t0+W1+W2+
+      f(yearID, model="iid", prior="normal",param=c(0,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea.t0, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))
+  }
+  if(ncol(crowd)==3) {
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    W3 <- crowd[,3]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    D$yearW3 <- 100+D$yearW2 # for random year effect on crowding
+    
+    formula <- logarea.t1 ~ logarea.t0+W1+W2+W3+
+      f(yearID, model="iid", prior="normal",param=c(0,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea.t0, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW3, W3, model="iid", prior="normal",param=c(0,0.001))
+  }
+  if(ncol(crowd)==4) {
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    W3 <- crowd[,3]
+    W4 <- crowd[,4]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    D$yearW3 <- 100+D$yearW2 # for random year effect on crowding
+    D$yearW4 <- 100+D$yearW3 # for random year effect on crowding
+    
+    formula <- logarea.t1 ~ logarea.t0+W1+W2+W3+W4+
+      f(yearID, model="iid", prior="normal",param=c(0,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea.t0, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW3, W3, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW4, W4, model="iid", prior="normal",param=c(0,0.001))
+  }
+  
+  outINLA <- inla(formula, data=D,
+                  family=c("gaussian"), verbose=FALSE,
+                  control.predictor = list(link = 1),
+                  control.compute=list(dic=T,mlik=T),
+                  control.inla = list(h = 1e-10),
+                  control.fixed = list(correlation.matrix=TRUE))
+  
+#   tmp_fixed_covariance <- outINLA$misc$lincomb.derived.correlation.matrix
+#   
+#   # Fit variance
+#   x <- outINLA$summary.fitted.values$mean #fitted values from INLA
+#   y <- (D$logarea.t1-outINLA$summary.fitted.values$mean)^2 #calculates the variance (residuals^2)
+#   outVar <- nls(y~a*exp(b*x),start=list(a=1,b=0)) #model the variance as function of fitted size
+  
+  # Collect parameters
+  # random year and group effects
+  if(ncol(crowd)==2){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr")
+  }
+  if(ncol(crowd)==3){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    params <- cbind(params, outINLA$summary.random$yearW3[,2])
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr", "W3.yr")
+  }
+  if(ncol(crowd)==4){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    params <- cbind(params, outINLA$summary.random$yearW3[,2])
+    params <- cbind(params, outINLA$summary.random$yearW4[,2])
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr", "W3.yr", "W4.yr")
+  }
+  
+  tmp <- as.data.frame(outINLA$summary.random$GroupID[,2])
+  names(tmp) <- "Group"
+  tmp$GrpName <- outINLA$summary.random$GroupID[,1]
+  tmp[(NROW(tmp)+1):NROW(params),]=NA
+  params=cbind(params,tmp)
+  
+  # fixed effects
+  fixed <- as.data.frame(outINLA$summary.fixed)[,1:2]
+  tmp=matrix(NA,dim(params)[1],nrow(fixed))
+  colnames(tmp)=rownames(fixed)
+  tmp[1,]=fixed[,1]
+  params=cbind(params,tmp)
+  colnames(params)[6] <- "Intercept"
+  params$alpha=NA; params$alpha[1:length(alpha)]=alpha
+#   #variance 
+#   params$sigma.a=NA; params$sigma.a[1]=coef(outVar)[1] 
+#   params$sigma.b=NA; params$sigma.b[1]=coef(outVar)[2]
+  
+  return(params)
+}
+
+
+
+
+
+
+#' Estimate survival regression coefficients using INLA
+#' 
+#' @author Andrew Tredennick
+#' @param dataframe Time series dataframe of genet sizes at t0 and t1 with 
+#'                  neighborhood crowding covariate for each observation.
+#' @param crowd_mat Matrix of crowding indices for each observation. Matrix
+#'                  dimensions are nrow = number of observations,
+#'                  ncol = number of species.
+#' @param alphas Vector of length(species) of vital rate specific alpha values.
+#' @return Dataframe with named regression coefficients.
+
+get_survival_params_yrlcomp <- function(dataframe, crowd_mat, alpha){
+  D <- dataframe
+  D$logarea <- log(D$area)
+  D$quad <- as.character(D$quad)
+  
+  crowd = crowd_mat 
+  crowd[crowd<1e-99]=0 # make really small crowding indices 0
+  
+  library(INLA)
+  # Set up ID variables for INLA random effects
+  D$yearID <- D$year+max(D$year) # for random year offset on intercept
+  D$GroupID <- as.numeric(D$Group)
+  
+  if(ncol(crowd)==2){
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    
+    formula2 <- survives ~ logarea+W1+W2+
+      f(yearID, model="iid", prior="normal",param=c(1,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))
+  }
+  if(ncol(crowd)==3){
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    W2 <- crowd[,3]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    D$yearW3 <- 100+D$yearW2 # for random year effect on crowding
+    
+    formula2 <- survives ~ logarea+W1+W2+W3+
+      f(yearID, model="iid", prior="normal",param=c(1,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW3, W3, model="iid", prior="normal",param=c(0,0.001))
+  }
+  if(ncol(crowd)==4){
+    W1 <- crowd[,1]
+    W2 <- crowd[,2]
+    W3 <- crowd[,3]
+    W4 <- crowd[,4]
+    D$yearW1 <- 100+D$yearID # for random year effect on crowding
+    D$yearW2 <- 100+D$yearW1 # for random year effect on crowding
+    D$yearW3 <- 100+D$yearW2 # for random year effect on crowding
+    D$yearW4 <- 100+D$yearW3 # for random year effect on crowding
+    
+    formula2 <- survives ~ logarea+W1+W2+W3+W4+
+      f(yearID, model="iid", prior="normal",param=c(1,0.001))+
+      f(GroupID, model="iid", prior="normal",param=c(0,0.001))+
+      f(year, logarea, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW1, W1, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW2, W2, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW3, W3, model="iid", prior="normal",param=c(0,0.001))+
+      f(yearW4, W4, model="iid", prior="normal",param=c(0,0.001))
+  }
+  
+  outINLA <- inla(formula2, data=D,
+                  family=c("binomial"), verbose=FALSE,
+                  control.compute=list(dic=T,mlik=T),
+                  control.predictor = list(link = 1),
+                  control.inla = list(h = 1e-10),
+                  Ntrials=rep(1,nrow(D)),
+                  control.fixed = list(correlation.matrix=TRUE))
+  
+  # tmp_fixed_covariance <- outINLA$misc$lincomb.derived.correlation.matrix
+  
+  #Collect parameters
+  #random year and group effects
+  if(ncol(crowd)==2){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    ngroups <- length(outINLA$summary.random$GroupID[,2])
+    params <- cbind(params, c(outINLA$summary.random$GroupID[,2],rep(NA,nrow(params)-ngroups)))
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr", "Group")
+  }
+  if(ncol(crowd)==3){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    params <- cbind(params, outINLA$summary.random$yearW3[,2])
+    ngroups <- length(outINLA$summary.random$GroupID[,2])
+    params <- cbind(params, c(outINLA$summary.random$GroupID[,2],rep(NA,nrow(params)-ngroups)))
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr", "W3.yr", "Group")
+  }
+  if(ncol(crowd)==4){
+    params <- as.data.frame(outINLA$summary.random$yearID[,1:2])
+    params <- cbind(params, outINLA$summary.random$year[,2])
+    params <- cbind(params, outINLA$summary.random$yearW1[,2])
+    params <- cbind(params, outINLA$summary.random$yearW2[,2])
+    params <- cbind(params, outINLA$summary.random$yearW3[,2])
+    params <- cbind(params, outINLA$summary.random$yearW4[,2])
+    ngroups <- length(outINLA$summary.random$GroupID[,2])
+    params <- cbind(params, c(outINLA$summary.random$GroupID[,2],rep(NA,nrow(params)-ngroups)))
+    names(params) <- c("Year", "Intercept.yr", "logarea.t0.yr", 
+                       "W1.yr", "W2.yr","W3.yr","W4.yr", "Group")
+  }
+ 
+  #fixed effects
+  fixed <- as.data.frame(outINLA$summary.fixed)[,1:2]
+  tmp=matrix(NA,dim(params)[1],nrow(fixed))
+  colnames(tmp)=rownames(fixed)
+  tmp[1,]=fixed[,1]
+  params=cbind(params,tmp)
+  params$alpha=NA; params$alpha[1:length(alpha)]=alpha
+  colnames(params)[which(colnames(params)=="(Intercept)")] <- "Intercept"
+  
+  return(params)
+}
+
+
+
+
+
+
---
+++ runscripts/call_vitalrate_fxns_yrlcomp.R
@@ -0,0 +1,187 @@
+##  Script to source vital rate regression functions
+
+rm(list=ls(all=TRUE))
+library(communitySynchrony)
+
+# Set up some paths and lists
+path_to_data <- "../data/"
+site_list <- list.files(path_to_data)
+removes <- c(grep("*.csv", site_list),
+             grep("Climate", site_list),
+             grep("Crowding", site_list))
+site_list <- site_list[-removes]
+
+# Load all crowding data list
+crowd_growth <- readRDS(paste(path_to_data, "Crowding/crowd_grow_list.RDS", sep=""))
+crowd_surv <- readRDS(paste(path_to_data, "Crowding/crowd_survival_list.RDS", sep=""))
+
+# Load all alphas
+grow_alphas <- read.csv(paste(path_to_data, "alpha_list_growth.csv", sep=""))
+surv_alphas <- read.csv(paste(path_to_data, "alpha_list_survival.csv", sep=""))
+
+####
+#### GROWTH -------------------------------------------------------------------
+####
+# Loop through sites and then species
+growth_params_biglist <- list()
+for(do_site in site_list){
+  species_list <- list.files(paste(path_to_data, do_site, "/", sep=""))
+  alpha_now <- subset(grow_alphas, Site==do_site)
+  alpha_now <- as.numeric(alpha_now$Alpha)
+  
+  growth_params <- list()
+  for(do_species in species_list){
+    growDfile <- paste(path_to_data, do_site, "/", do_species,"/growDnoNA.csv",sep="")
+    growD <- read.csv(growDfile)
+    #TODO -- check with Peter about this allEdge subset
+    D <- growD  #subset(growD,allEdge==0)
+    D$logarea.t0 <- log(D$area.t0)
+    D$logarea.t1 <- log(D$area.t1)
+    D$quad <- as.character(D$quad)
+    
+    # Add group info for each site individually, as needed
+    if(do_site=="Arizona"){
+      D$Group=as.factor(substr(D$quad,1,1))
+      D=subset(D,year>=17)
+    }
+    
+    if(do_site=="Kansas")
+      D$Group=as.numeric(D$Group)-1
+    
+    if(do_site=="Montana"){
+      ##then we moved some specific points:
+      tmp2<-which(D$quad=="A12" & D$year==44)
+      tmp3<-which(D$quad=="B1"  & D$year==44)
+      tmp41<-which(D$quad=="E4" & D$year==33) 
+      tmp42<-which(D$quad=="E4" & D$year==34) 
+      tmp43<-which(D$quad=="E4" & D$year==43)
+      tmp44<-which(D$quad=="E4" & D$year==44)
+      D$Group=as.factor(substr(D$quad,1,1)) 
+    }
+    
+    if(do_site=="NewMexico")
+      D$Group=as.factor(substr(D$quad,1,1))
+    
+    # Get the years right for Kansas
+    if(do_site=="Kansas"){
+      D <- subset(D, year<68)
+      ##to remove some points:
+      #for q25
+      tmp1<-which(D$quad=="q25")
+      #for q27
+      tmp2<-which(D$quad=="q27")
+      #for q28
+      tmp3<-which(D$quad=="q28")
+      #for q30
+      tmp4<-which(D$quad=="q30")
+      #for q31
+      tmp5<-which(D$quad=="q31" & (D$year<35 | D$year>39))
+      #for q32
+      tmp6<-which(D$quad=="q32" & (D$year<35 | D$year>41))
+      tmp<-c(tmp1,tmp2,tmp3,tmp4,tmp5,tmp6)
+      D<-D[-tmp,]
+    }
+      
+    # Get correct crowding matrix
+    crowd_growth_now <- crowd_growth[[do_site]][[do_species]]
+    tmpmerge <- merge(D, crowd_growth_now, by.x="X", by.y="xID")
+    tokeep <- grep("V", colnames(tmpmerge))
+    crowd_growth_now <- as.matrix(tmpmerge[,tokeep])
+    
+    # Run through the function
+    tmp <- get_growth_params_yrlcomp(dataframe = D,
+                             crowd_mat = crowd_growth_now,
+                             alpha = alpha_now)
+    
+    # Save in temporary list
+    growth_params[[do_species]] <- tmp
+  } #end species loop
+  # Save to the big list
+  growth_params_biglist[[do_site]] <- growth_params
+} #end site loop
+
+# Save the big parameter list
+saveRDS(growth_params_biglist, "../results/growth_params_yrlycomp_list.RDS")
+
+
+
+####
+#### SURVIVAL -----------------------------------------------------------------
+####
+# Loop through sites and then species
+surv_params_biglist <- list()
+for(do_site in site_list){
+  species_list <- list.files(paste(path_to_data, do_site, "/", sep=""))
+  alpha_now <- subset(surv_alphas, Site==do_site)
+  alpha_now <- as.numeric(alpha_now$Alpha)
+  
+  surv_params <- list()
+  for(do_species in species_list){
+    survDfile <- paste(path_to_data, do_site, "/", do_species,"/survD.csv",sep="")
+    survD <- read.csv(survDfile)
+    #TODO -- check with Peter about this allEdge subset
+    D <- survD  #subset(growD,allEdge==0)
+    D$logarea <- log(D$area)
+    D$quad <- as.character(D$quad)
+    
+    # Add group info for each site individually, as needed
+    if(do_site=="Arizona"){
+      D$Group=as.factor(substr(D$quad,1,1))
+      D=subset(D,year>=17)
+    }
+    if(do_site=="Kansas")
+      D$Group=as.numeric(D$Group)-1
+    if(do_site=="Montana"){
+      ##then we moved some specific points:
+      tmp2<-which(D$quad=="A12" & D$year==44)
+      tmp3<-which(D$quad=="B1"  & D$year==44)
+      tmp41<-which(D$quad=="E4" & D$year==33) 
+      tmp42<-which(D$quad=="E4" & D$year==34) 
+      tmp43<-which(D$quad=="E4" & D$year==43)
+      tmp44<-which(D$quad=="E4" & D$year==44)
+      D$Group=as.factor(substr(D$quad,1,1)) 
+    }
+    if(do_site=="NewMexico")
+      D$Group=as.factor(substr(D$quad,1,1))
+    
+    # Get the years right for Kansas
+    if(do_site=="Kansas"){
+      D <- subset(D, year<68)
+      ##to remove some points:
+      #for q25
+      tmp1<-which(D$quad=="q25")
+      #for q27
+      tmp2<-which(D$quad=="q27")
+      #for q28
+      tmp3<-which(D$quad=="q28")
+      #for q30
+      tmp4<-which(D$quad=="q30")
+      #for q31
+      tmp5<-which(D$quad=="q31" & (D$year<35 | D$year>39))
+      #for q32
+      tmp6<-which(D$quad=="q32" & (D$year<35 | D$year>41))
+      tmp<-c(tmp1,tmp2,tmp3,tmp4,tmp5,tmp6)
+      D<-D[-tmp,]
+    }
+    
+    # Get correct crowding matrix
+    crowd_surv_now <- crowd_surv[[do_site]][[do_species]]
+    tmpmerge <- merge(D, crowd_surv_now, by.x="X", by.y="xID")
+    tokeep <- grep("V", colnames(tmpmerge))
+    crowd_surv_now <- as.matrix(tmpmerge[,tokeep])
+    
+    # Run through the function
+    tmp <- get_survival_params_yrlcomp(dataframe = D,
+                               crowd_mat = crowd_surv_now,
+                               alpha = alpha_now)
+    
+    # Save in temporary list
+    surv_params[[do_species]] <- tmp
+  } #end species loop
+  # Save to the big list
+  surv_params_biglist[[do_site]] <- surv_params
+} #end site loop
+
+# Save the big parameter list
+saveRDS(surv_params_biglist, "../results/surv_params_yrlycomp_list.RDS")
+
