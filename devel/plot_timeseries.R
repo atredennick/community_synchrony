088f457228acfd99f3d400c2ade9624ddd425eab
Andrew Tredennick <atredenn@gmail.com>
2016-11-03 17:05:16 -0600

    more revisions; figure updates

--- devel/plot_timeseries.R
+++ devel/plot_timeseries.R
@@ -7,5 +7,25 @@ rm(list=ls())
 par(mfrow=c(1,2))
 ibms <- readRDS("../results/ibm_sims/ibm_Kansas_constnointerexpand1.RDS")
 matplot(ibms[,c(4:5)]*100, type="l")
-ibms <- readRDS("../results/ibm_sims/ibm_Kansas_constnointerexpand5.RDS")
-matplot(ibms[,c(4:5)]*100, type="l")
+ibms <- readRDS("../results/ibm_sims/ibm_Montana_fluctnointerexpand5.RDS")
+matplot(ibms[101:200,c(4:7)]*100, type="l", lty = 1, lwd=2)
+
+# caclulate observed growth rates
+# create lagged data frame to only get observed yearly transitions
+ts_mat <- as.data.frame(ibms[1:100,c(2,4:7)])
+lag_df <- ts_mat
+lag_df$lagyear <- lag_df$time+1
+num_spp=4
+species_list <- c("Cov.BOGR","Cov.HECO","Cov.PASM","Cov.POSE")
+colnames(lag_df)[2:(num_spp+1)] <- paste(colnames(lag_df)[2:(num_spp+1)],"_t0", sep="") 
+# merge the lag df with observed
+rm_col <- which(colnames(lag_df)=="time")
+merged_df <- merge(ts_mat, lag_df[,-rm_col], by.x = "time", by.y="lagyear")
+transitions <- nrow(merged_df)
+obs_gr <- matrix(nrow=transitions, ncol=num_spp)
+name_ids1 <- which(colnames(merged_df) %in% species_list)
+name_ids2 <- which(colnames(merged_df) %in% paste(species_list, "_t0", sep=""))
+for(i in 1:transitions){
+  obs_gr[i,] <- as.numeric(log(merged_df[i,name_ids1]/merged_df[i,name_ids2]))
+}
+matplot(obs_gr, type="l", lty = 1, lwd=2)
